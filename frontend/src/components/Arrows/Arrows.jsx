13294ef857f5b9388de6b43c385841dadf2481ec
angyaguilr <aaguilar@elon.edu>
2025-02-20 00:56:20 -0500

    mobile changes to website

--- frontend/src/components/Arrows/Arrows.jsx
+++ frontend/src/components/Arrows/Arrows.jsx
@@ -1,7 +1,7 @@
 
 const Arrows = () => {
    return (
-      <div className="animate-bounce absolute bottom-32 z-50">
+      <div className="animate-bounce absolute bottom-14 z-50">
          <img src="/assets/images/arrows-icon.png" alt="Icono flechas" />
       </div>
    )
--- frontend/src/components/MainHeadline/MainHeadline.jsx
+++ frontend/src/components/MainHeadline/MainHeadline.jsx
@@ -1,6 +1,6 @@
 const MainHeadline = () => {
    return (
-      <section className="z-50 flex flex-col items-center gap-5 absolute inset-0 mx-auto top-[40%] lg:top-[35%]">
+      <section className="z-50 flex flex-col items-center gap-5 absolute inset-0 mx-auto top-[36%] lg:top-[35%]">
          <img src="/assets/images/header-title-mobile.png" alt="" className="w-52 mb-2 lg:w-64" />
          <img src="/assets/images/names.png" alt="" className="w-72 lg:w-96"/>
          <hr className="w-44 border-3 border-mustard " />
