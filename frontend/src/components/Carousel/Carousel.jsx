b641d32e15ad6f4e5fbdfca28c3e620dfd093abf
angyaguilr <aaguilar@elon.edu>
2025-02-20 00:37:08 -0500

    small fix to img

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -4,7 +4,7 @@ const Carousel = () => {
    return (
       <section className='flex justify-center items-baseline relative z-40 w-[450px] h-72 pt-12 overflow-hidden
          lg:w-full lg:h-96 lg:pt-6 '>
-         < img src="/assets/photos/img-01.PNG" className="pic01"/>
+         < img src="/assets/photos/img-01.png" className="pic01"/>
          < img src="/assets/photos/img-02.png" className="pic02"/>
          < img src="/assets/photos/img-03.png" className="pic03"/>
          < img src="/assets/photos/img-04.png" className="pic04"/>
